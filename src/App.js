e2129c5c22d57a8415eb5bbace1c313582cd7b86
Kajalgupta1011 <guptakajal040917@gmail.com>
2023-04-06 13:46:43 +0530

    code for seat booking

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import BookingSeat, {bookedSeats} from "./BookingSeat";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="form">
-        <h1 className='heading'>
-          Online Seat's Booking
-        </h1>
-        <label htmlFor="numSeat"></label>
-        <input type="number" />
-        <small>
-            error
-        </small>
-        <p>Available Seats : </p>
-        <button>Book Ticket</button>
-        <div className="identify">
-          <div className="avlSeats"></div>
-          <p>Available Seats</p>
-          <div className="bookedSeats"></div>
-          <p>Booked Seats</p>
-        </div>
-      </div>
-    </div>
-  );
+	const avalSeats = 80 - bookedSeats.length;
+	const [seats, setSeats] = useState("");
+	const [error, setError] = useState("");
+
+	function handleSubmit() {
+
+		if (!seats) return setError("Please enter the number of seats !!!");
+		if (seats <= 0) return setError("Seat number can't be 0");
+		if (seats > 7) return setError("Seat number can't be greater than 7");
+		if(avalSeats < seats)	return setError("Value exceeds available seats")
+
+		console.log(BookingSeat(Number(seats)));
+
+		setError("");
+		setSeats("");
+	}
+	return (
+		<div className="container">
+			<div className="form">
+				<h1 className="heading">Online Seat's Booking</h1>
+				<p>Enter number of Seats</p>
+				<input
+					type="number"
+					value={seats}
+					onChange={(e) => setSeats(e.target.value)}
+					min={0}
+				/>
+				<small>{error}</small>
+				<p>Available Seats : {avalSeats}</p>
+				<button onClick={handleSubmit}>Book Ticket</button>
+				<div className="identify">
+					<div className="avlSeats"></div>
+					<p>Available Seats</p>
+					<div className="bookedSeats"></div>
+					<p>Booked Seats</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
