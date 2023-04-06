5c465ba639c8f0a385c33b283dcd96838f7f7c9a
Kajalgupta1011 <guptakajal040917@gmail.com>
2023-04-06 12:59:37 +0530

    basic structure for input

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      
+      <div className="form">
+        <h1 className='heading'>
+          Online Seat's Booking
+        </h1>
+        <label htmlFor="numSeat"></label>
+        <input type="number" />
+        <small>
+            error
+        </small>
+        <p>Available Seats : </p>
+        <button>Book Ticket</button>
+        <div className="identify">
+          <div className="avlSeats"></div>
+          <p>Available Seats</p>
+          <div className="bookedSeats"></div>
+          <p>Booked Seats</p>
+        </div>
+      </div>
     </div>
   );
 }
